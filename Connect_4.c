e0eb3f06c4d199a1a2a298b1941fdd7b48e4df96
Slasher6157 <89289754+Zachadactyl@users.noreply.github.com>
2026-03-03 11:41:00 -0700

    Added starter C file

---
+++ Connect_4.c
@@ -0,0 +1,19 @@
+/*
+ * File:        Connect_4.c
+ * Authors:     Ashlee Santos, Zachary Tull, Angelo Valencia
+ * Purpose:     Demonstrate teamwork, problem solving, time management, and a moderate understanding of C coding in large-scale projects.
+ * 
+ * Date:        3 March 2026
+ *
+ * Sources of Help: N/A so far
+ */
+
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+
+
+
+    return 0;
+}
