085bafe7895b5544d6eded88e789343e453eca59
Slasher6157 <89289754+Zachadactyl@users.noreply.github.com>
2026-04-02 10:08:14 -0700

    Added menu + simplified playGame()
    Added a menu to sort through items, merged file I/O into functions and connections.h, updated main() to use playGame() function, and added more prototypes

--- Connect_4.c
+++ Connect_4.c
@@ -1,39 +1,61 @@
 /*
  * File:        Connect_4.c
  * Authors:     Ashlee Santos, Zachary Tull, Angelo Valencia
- * Purpose:     Demonstrate teamwork, problem solving, time management, and a moderate understanding of C coding in large-scale projects.
+ * Purpose:     A beginner-friendly implementation of Connect 4.
+ * Date:        3 March 2026 
  * 
- * Date:        3 March 2026
- *
- * Sources of Help: N/A so far
+ * Sources of Help: Canvas, W3 Schools, Stack Exchange
  */
 
 #include <stdio.h>
-#include <time.h>
-#define ROWS 6
-#define COLS 7
+#include "connections.h"
+
 
 /* Function Prototypes */
-void initBoard();
-void printBoard();
-void dropPiece();
-
+void initBoard(char board[ROWS][COLS]);
+void printBoard(char board[ROWS][COLS]);
+int dropPiece(char board[ROWS][COLS], int col, char mark);
+void playGame();
+void loadLeaderboard(void);
+void sortLeaderboard(void);
+void displayLeaderboard(void);
+int getPlayerInfo(char* name);
+void saveLeaderBoard(void);
 
 int main() {
-
-printf("Hello\n");
-
+    int caseNum = 0;
+    
+    
+    printf("\n\033[33mWELCOME\033[0m \033[31mTO\033[0m \033[33mCONNECT\033[0m \033[31m4!\033[0m\n\n"); /* WELCOME TO CONNECT 4! (in red and yellow caps) */
+    printf("1. Play the game!\n");
+    printf("2. View Leaderboard!\n");
+    printf("3. Get Help!\n");
+    printf("4. Exit Game :(\n\n");
+    
+    printf("Choose an option: ");
+    scanf("%d", &caseNum);
+    
+    switch (caseNum){
+        case 1: 
+            playGame();
+        break;
+    case 2: 
+        printf("Put leaderbord here or sum...\n");
+        loadLeaderboard();
+        sortLeaderboard();
+        displayLeaderboard();
+        break;
+    case 3:
+        printf("Working on instructions...\n");
+        break;
+    case 4:
+        printf("Thanks for playing!!\n\n");
+        break;
+    }
+  
+    
     return 0;
 }
 
-/* Function Definitions */
 
-void startBoard() {
-    printf(" | 1 | 2 | 3 | 4 | 5 | 6 | 7 | ");
-    printf(" |___|___|___|___|___|___|___| ");
-    printf(" |___|___|___|___|___|___|___| ");
-    printf(" |___|___|___|___|___|___|___| ");
-    printf(" |___|___|___|___|___|___|___| ");
-    printf(" |___|___|___|___|___|___|___| ");
-    printf(" |___|___|___|___|___|___|___| ");
-}
+
